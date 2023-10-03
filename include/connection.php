82983d845e30d4fe8b9a17d682013a56d5968bec
Mark Piolo Pascual <piolopascual410@gmail.com>
2023-10-04 07:41:12 +0800

    Connection Updated

--- include/connection.php
+++ include/connection.php
@@ -1,12 +1,18 @@
 <?php
-$dbServer = "easecholar.mysql.database.azure.com";
-$dbUsername = "easecholar";
-$dbPassword = "IphonenaMaarte0208";
-$dbName = "easecholar";
+$serverName = "easecholar.mysql.database.azure.com";
+$databaseName = "easecholar"; // Replace with your actual database name
+$username = "easecholar";
+$password = "your-password"; // Replace with your actual password
 
-$dbConn = mysqli_connect($dbServer, $dbUsername, $dbPassword, $dbName);
+// Construct the connection string
+$connectionString = "Server=$serverName;Database=$databaseName;User Id=$username;Password=$password;";
 
-if (!$dbConn) {
-    die("Connection failed: " . mysqli_connect_error());
+// Create a database connection
+$conn = new mysqli($serverName, $username, $password, $databaseName);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
