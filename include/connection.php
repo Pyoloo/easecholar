ee7146dc6c427d0a8ba787023bc8a035b1c74178
Mark Piolo Pascual <piolopascual410@gmail.com>
2023-10-04 08:41:01 +0800

    Connection Update

--- include/connection.php
+++ include/connection.php
@@ -2,17 +2,18 @@
 $serverName = "easecholar.mysql.database.azure.com";
 $databaseName = "easecholar";
 $username = "easecholar";
-$password = "IphonenaMaarte0208"; 
+$password = "IphonenaMaarte0208";
 
-// Construct the connection string
-$connectionString = "Server=$serverName;Database=$databaseName;User Id=$username;Password=$password;";
+// Construct the connection string with SSL options
+$connectionString = "Server=$serverName;Database=$databaseName;User Id=$username;Password=$password;Encrypt=true;TrustServerCertificate=false";
 
-// Create a database connection
-$conn = new mysqli($serverName, $username, $password, $databaseName);
-
-// Check the connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Create a database connection with the mysqli_ssl_set function
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+if (!$conn) {
+    die("mysqli_init failed");
+}
+if (!$conn->real_connect($serverName, $username, $password, $databaseName, 3306, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Connect Error: " . mysqli_connect_error());
 }
 ?>
-
